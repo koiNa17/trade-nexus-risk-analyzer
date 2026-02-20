2edaca3593c8e727681b5837b6806d17b1008a76
koiNa17 <ryvl7826@yahoo.co.jp>
2026-02-20 19:20:15 +0000

    feat: 分析機関の選択（プルダウン）機能を追加

--- app.py
+++ app.py
@@ -6,8 +6,12 @@ st.write("金融データ分析ダッシュボード")
 
 st.subheader("USD/JPY(ドル円)過去1か月のデータ")
 tickers = {'ドル円': 'JPY=X', 'ユーロ円': 'EURJPY=X', '日経平均': '^N225'}
-name = st.selectbox("分析対象を選択してください:", list(tickers.keys()))
-data = yf.Ticker(tickers[name]).history(period="1mo")
+name = st.selectbox("分析対象を選択してください：", list(tickers.keys()))
+periods = {'1か月': '1mo', '半年': '6mo', '1年': '1y'}
+p_name = st.selectbox("期間を選択してください：", list(periods.keys()))
+data = yf.Ticker(tickers[name]).history(period=periods[p_name])
+
+# data = yf.Ticker(tickers[name]).history(period="1mo")
 
 st.subheader(f"{name} のデータと推移")
 st.write(data)
